dcb7696ef9f8e0d4b6334e4b208d0e2caca4fa36
bddoylan <bddoylan@gmail.com>
2022-11-09 23:41:50 -0500

    + Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
+import RegisterPage from "./pages/RegisterPage";
+import MainPage from "./pages/MainPage";
+
+const App = () => {
+	return (
+		<Routes>
+			<Route path="/" element={<LandingPage />} />
+			<Route path="/login" element={<LoginPage />} />
+			<Route path="/register" element={<RegisterPage />} />
+			<Route path="/profile" element={<ProfilePage />} />
+			<Route path="/main" element={<MainPage />} />
+		</Routes>
+	);
+};
+
+export default App;
